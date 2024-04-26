aa7d5435ef430828cb9190c014452a99d5645e85
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-04-26 18:02:44 +0700

    Delete api-testing directory

--- api-testing/api_test/api_url.py
+++
@@ -1,3 +0,0 @@
-import function_base
-GET_URL = function_base.get_base_end_point() + "/public/v2/users"
-#GET_URL = function_base.get_base_end_point() + "/public/v2/posts"
--- api-testing/api_test/function_base.py
+++
@@ -1,8 +0,0 @@
-import json
-def get_base_end_point():
-    with open("C:\\api-testing\\config\\endpoint.json","r") as json_file:
-        properties = json.load(json_file)
-        env = properties["environment"]["env"]
-        return properties[env]["base_url"]
-    
-    
